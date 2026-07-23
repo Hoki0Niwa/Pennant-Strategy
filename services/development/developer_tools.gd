d2387388d059c9fad42dcaa9e88c9981027ecd78
Hoki0Niwa <50765794+Hoki0Niwa@users.noreply.github.com>
2026-07-23 12:22:27 +0900

    配布の準備

--- services/development/developer_tools.gd
+++ services/development/developer_tools.gd
@@ -2,10 +2,10 @@ extends RefCounted
 class_name PSDeveloperTools
 
 # 開発者向け UI の表示スイッチ。
-# project.godot の ProjectSettings 値だけを見て、セーブデータには混ぜない。
+# デバッグ実行かつ ProjectSettings が有効な場合だけ表示し、セーブデータには混ぜない。
 const SETTING_KEY: String = "pennant_strategy/development/show_tools"
 
 
 # true のときだけテストモードや調査用画面への導線を表示する。
 static func enabled() -> bool:
-	return bool(ProjectSettings.get_setting(SETTING_KEY, false))
+	return OS.is_debug_build() and bool(ProjectSettings.get_setting(SETTING_KEY, false))
